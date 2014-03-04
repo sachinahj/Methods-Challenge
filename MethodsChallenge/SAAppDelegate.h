da2da3138c3a848b722f31d5d9a311b67f511e64
Sachin Ahuja <Sachin@sachin-ahujas-macbook-pro.local>
2014-03-04 16:55:24 -0600

    Initial Commit

---
+++ MethodsChallenge/SAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAAppDelegate.h
+//  MethodsChallenge
+//
+//  Created by Sachin Ahuja on 3/4/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MethodsChallenge/SAViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAViewController.h
+//  MethodsChallenge
+//
+//  Created by Sachin Ahuja on 3/4/14.
+//  Copyright (c) 2014 Sachin Ahuja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAViewController : UIViewController
+
+@end
